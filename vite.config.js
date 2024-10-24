9463d015a859bba1600f2f39d4d6778095f78ef4
alexanderdombroski <dom21013@byui.edu>
2024-10-24 12:49:25 -0600

    added base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import deno from '@deno/vite-plugin'
 // https://vite.dev/config/
 export default defineConfig({
   root: 'src',
+  base: '/vite-project/',
   build: {
     outDir: '../dist',
     emptyOutDir: true
