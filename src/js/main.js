fea2fdc7e925b971abf05496955091d292f1bae6
alexanderdombroski <dom21013@byui.edu>
2024-10-24 12:31:44 -0600

    feat!: changed file organization for deployment

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import '../css/style.css';
+import javascriptLogo from '../assets/javascript.svg';
+import denoViteLogo from '../assets/vite-deno.svg';
+import viteLogo from '../assets/vite.svg';
+import { setupCounter } from './counter.js';
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <img src="/vite-deno.svg" alt="Vite with Deno" />
+    <img src="${denoViteLogo}" alt="Vite with Deno" />
     <div>
       <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
+        <img src="${viteLogo}" class="logo" alt="Vite logo" />
       </a>
       <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
         <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import deno from '@deno/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   plugins: [deno()],
 })
