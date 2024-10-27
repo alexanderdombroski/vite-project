ee55547d771db33eede498a2e175b32c3de33f35
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2024-10-26 22:27:46 -0600

    Update style.css and index.html
    Adjust styles in style.css for the header, home_content, and footer sections.
    Add media queries for different screen sizes.
    Update index.html to include a script for changing the hero image based on screen size.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
+let heroImage = document.querySelector('#home-hero-image');
 
+
+function changeImage() {
+    if (window.innerWidth < 1000) {
+        heroImage.src = './images/planner2.avif';
+    } else {
+        heroImage.src = './images/planner1.webp';
+    }
+}
+
+function init(){
+    window.addEventListener('resize', changeImage);
+    window.addEventListener('load', changeImage);
+    window.addEventListener('DOMContentLoaded', changeImage);
+}
+
+init();
