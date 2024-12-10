7990c8e396620a0f344f811e1595337da236bdbf
alexanderdombroski <dom21013@byui.edu>
2024-12-10 09:29:23 -0700

    feat: added toTitle str util func

--- src/js/utils/str.mjs
+++ src/js/utils/str.mjs
@@ -1,4 +1,4 @@
 const capitalize = str => str ? str[0].toUpperCase() + str.slice(1).toLowerCase() : '';
+const toTitleCase = str => str ? str.split(' ').map(capitalize).join(' ') : '';
 
-
-export { capitalize }
+export { capitalize, toTitleCase }
